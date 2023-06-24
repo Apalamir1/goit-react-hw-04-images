d6b237eb334ef4277bcd05bf2aaaf1f22019bb60
OleksandrZaitsev <apalamir17@gmail.com>
2023-06-24 21:04:13 +0300

    searchBar

--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -1,46 +1,43 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import IconBtn from 'components/IconBtn/IconBtn';
 import { ReactComponent as SearchIcon } from '../icons/image_search.svg';
 import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 
-export default class Searchbar extends Component {
-  state = { query: '' };
+export default function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
 
-  handleInputChange = event => {
+  const handleInputChange = event => {
     const { value } = event.currentTarget;
 
-    this.setState({ query: value });
+    setQuery(value);
   };
-  handleInputSubmit = event => {
+  const handleInputSubmit = event => {
     event.preventDefault();
-    const { query } = this.state;
-    const { onSubmit } = this.props;
+
     onSubmit(query.toLowerCase().trim());
-    this.setState({ query: '' });
+    setQuery('');
   };
 
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleInputSubmit}>
-          <IconBtn type="submit" className={css.SearchFormButton}>
-            <SearchIcon className={css.svgIcon} />
-            <span className={css.SearchFormButtonLabel}></span>
-          </IconBtn>
-          <input
-            className={css.SearchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos..."
-            value={this.state.query}
-            onChange={this.handleInputChange}
-          />
-        </form>
-      </header>
-    );
-  }
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleInputSubmit}>
+        <IconBtn type="submit" className={css.SearchFormButton}>
+          <SearchIcon className={css.svgIcon} />
+          <span className={css.SearchFormButtonLabel}></span>
+        </IconBtn>
+        <input
+          className={css.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos..."
+          value={query}
+          onChange={handleInputChange}
+        />
+      </form>
+    </header>
+  );
 }
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
